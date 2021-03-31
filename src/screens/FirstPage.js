38d8af2e2f6dc25db34f21e3f1d10f44dea5b492
jadensiow <jianhong93@gmail.com>
2021-03-31 18:08:46 +0800

    minor changes to text

--- src/screens/FirstPage.js
+++ src/screens/FirstPage.js
@@ -9,6 +9,12 @@ export const FirstPage = () => {
       <span class="text3">Tracker</span>
 
       <h1 class="text4">Track all your entertainment under one site !!</h1>
+      <h3 class="text5">
+        {" "}
+        Track each categories of where have you stopped or completed. <br></br>
+        Add titles into your list by going to the Search and adding them into
+        either Watch List or Completed
+      </h3>
     </div>
   );
 };
